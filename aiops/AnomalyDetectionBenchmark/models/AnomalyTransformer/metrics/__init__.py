4f8d2bfe12475611d4dbfb857b8920b7099e7b9b
凌可 <penglanshu.pls@alibaba-inc.com>
2023-08-23 11:42:30 +0800

    delete unuse files

--- aiops/AnomalyDetectionBenchmark/models/AnomalyTransformer/metrics/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2023/8/23 11:19
-@Auther ： 凌可
-@File ：__init__.py.py
-"""
--- aiops/AnomalyDetectionBenchmark/models/usad/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2023/8/23 11:22
-@Auther ： 凌可
-@File ：__init__.py.py
-"""
