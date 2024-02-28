a6e2d023ff459d8a7b15d8280c1c0daf0c090f25
凌可 <penglanshu.pls@alibaba-inc.com>
2024-02-28 11:34:38 +0800

    delete

--- aiops/AnomalyDetectionBenchmark/models/DCdetector/scripts/NIPS_TS_Syn_Unvar.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-
-
-python main.py --anormly_ratio 3 --num_epochs 3   --batch_size 256  --mode train --dataset NIPS_TS_Syn_Unvar  --data_path NIPS_TS_Syn_Unvar  --input_c 1 --output_c 1  --loss_fuc MSE  
-python main.py --anormly_ratio 3  --num_epochs 10     --batch_size 256   --mode test    --dataset NIPS_TS_Syn_Unvar   --data_path NIPS_TS_Syn_Unvar --input_c 1    --output_c 1    --loss_fuc MSE    
-
