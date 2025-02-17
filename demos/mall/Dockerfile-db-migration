a60432bedf802f32d1ccebd176dd961776bfcdfb
jiongen.zje <jiongen.zje@alibaba-inc.com>
2025-02-17 12:05:57 +0800

    remove mall

--- demos/mall/Dockerfile-db-migration
+++
@@ -1,5 +0,0 @@
-FROM sreworks-registry.cn-beijing.cr.aliyuncs.com/sreworks/sw-migrate:latest
-COPY ./db /sql
-COPY ./db_init.sh /db_init.sh
-RUN chmod +x /db_init.sh
-ENTRYPOINT ["/db_init.sh"]
--- demos/mall/src/main/java/priv/jesse/mall/web/user/ClassificationController.java
+++
@@ -1,9 +0,0 @@
-package priv.jesse.mall.web.user;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/classification")
-public class ClassificationController {
-}
