45d7eb445b5e0f41cf15b3c107ef700ead5e4cd6
Twwy <jiongen.zje@alibaba-inc.com>
2025-06-15 23:34:34 +0800

    Update __main__.py

--- agitops/agitops/agent/explorer/__main__.py
+++ agitops/agitops/agent/explorer/__main__.py
@@ -49,7 +49,7 @@ class ExecutorAgent():
             with open(os.path.join(self.workspace_path, ".gitignore"), 'w') as f:
                 pass 
 
-        self.toolHandler = ToolHandler({"bash": "bash.py", "browser": "browser.py"},)
+        self.toolHandler = ToolHandler({"bash": "bash.py"})
 
         self.llm_client = OpenAI(
             api_key=self.conf["llm_api_key"],
